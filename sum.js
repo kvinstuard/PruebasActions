7dd351c894c0cae0bf5a332a38d95f9b141ca8bf
Kevin Marin <kevin.concha@correounivalle.edu.co>
2023-04-18 18:44:01 -0500

    testing again

--- sum.js
+++ sum.js
@@ -2,5 +2,10 @@ function sum(a, b) {
   return a + b;
 }
 
+function multiplication(a, b) {
+  return a * b;
+}
 
-module.exports = sum;
+
+
+module.exports = sum, multiplication;
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,11 @@
 const sum = require('./sum');
+const multiplication = require('./sum');
 
 test('sums two numbers', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test2('sums two numbers', () => {
+  expect(multiplication(1, 2)).toBe(2);
 });
 
