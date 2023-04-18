1fc10ebe5e65f0728cb3fc12f734b0d13d1c8693
Kevin Marin <kevin.concha@correounivalle.edu.co>
2023-04-18 18:44:51 -0500

    testing again

--- sum.test.js
+++ sum.test.js
@@ -5,7 +5,7 @@ test('sums two numbers', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test2('sums two numbers', () => {
+test('sums two numbers', () => {
   expect(multiplication(1, 2)).toBe(2);
 });
 
